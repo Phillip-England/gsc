4a0833fc576218a4374947cdb0e74e059dff1b9e
Phillip England <phillip.mark.england@gmail.com>
2024-08-30 05:26:00 -0500

    introduced Component type

--- gsc.go
+++ gsc.go
@@ -6,93 +6,161 @@ import (
 )
 
 //========================================
+//
+//========================================
+
+type Component string
+
+func NewComponent(s string, args ...string) Component {
+	if len(args) != 0 {
+		return Component(fmt.Sprintf(strings.TrimSpace(s), args))
+	} else {
+		return Component(strings.TrimSpace(s))
+	}
+}
+
+func (c Component) Text(text string) Component {
+	s := string(c)
+	s = strings.Replace(s, "><", `>`+text+`<`, 1)
+	return NewComponent(s)
+}
+
+func (c Component) Content(components ...Component) Component {
+	componentStr := ""
+	for _, component := range components {
+		componentStr = componentStr + string(component)
+	}
+	s := string(c)
+	s = strings.Replace(s, "><", `>`+componentStr+`<`, 1)
+	return NewComponent(s)
+}
+
+func (c Component) InsertAttr(attrName string, value string) Component {
+	strComponent := string(c)
+	rightBracketIndex := strings.Index(strComponent, ">")
+	if rightBracketIndex == -1 {
+		return ""
+	}
+	if string(strComponent[rightBracketIndex-1]) == "/" {
+		c = NewComponent(strings.Replace(strComponent, "/>", ` `+attrName+`='`+value+`'/>`, 1))
+	} else {
+		c = NewComponent(strings.Replace(strComponent, ">", ` `+attrName+`='`+value+`'>`, 1))
+	}
+	return c
+}
+
+func (c Component) Class(className string) Component {
+	c = Component(c.InsertAttr("class", className))
+	return c
+}
+
+func (c Component) Src(s string) Component {
+	c = Component(c.InsertAttr("src", s))
+	return c
+}
+
+func (c Component) Href(s string) Component {
+	c = Component(c.InsertAttr("href", s))
+	return c
+}
+
+func (c Component) Rel(s string) Component {
+	c = Component(c.InsertAttr("rel", s))
+	return c
+}
+
+func (c Component) Name(s string) Component {
+	c = Component(c.InsertAttr("name", s))
+	return c
+}
+
+//========================================
 // COMPONENTS
 //========================================
 
-func Link(attr string) string {
-	return fmt.Sprintf(`<link %s />`, attr)
+func Link() Component {
+	return NewComponent(`<link />`)
 }
 
-func Meta(content string) string {
-	return fmt.Sprintf(`<meta %s></meta>`, content)
+func Meta() Component {
+	return NewComponent(`<meta />`)
 }
 
-func P(attr string, text string) string {
-	return fmt.Sprintf(`<p %s>%s</p>`, attr, text)
+func P() Component {
+	return NewComponent(`<p></p>`)
 }
 
-func H1(attr string, text string) string {
-	return fmt.Sprintf(`<h1 %s>%s</h1>`, attr, text)
+func H1() Component {
+	return NewComponent(`<h1></h1>`)
 }
 
-func H2(attr string, text string) string {
-	return fmt.Sprintf(`<h2 %s>%s</h2>`, attr, text)
+func H2() Component {
+	return NewComponent(`<h2></h2>`)
 }
 
-func H3(attr string, text string) string {
-	return fmt.Sprintf(`<h3 %s>%s</h3>`, attr, text)
+func H3() Component {
+	return NewComponent(`<h3></h3>`)
 }
 
-func A(attr string, content ...string) string {
-	return fmt.Sprintf(`<a %s>%s</a>`, attr, strings.Join(content, "\n"))
+func A() Component {
+	return NewComponent(`<a></a>`)
 }
 
-func Head(attr string, title string, content ...string) string {
-	return fmt.Sprintf(`<head %s>%s<title>%s</title></head>`, attr, strings.Join(content, "\n"), title)
+func Head() Component {
+	return NewComponent(`<head></head>`)
 }
 
-func HTMLDoc(content ...string) string {
-	return fmt.Sprintf(`<!DOCTYPE><html>%s</html>`, strings.Join(content, "\n"))
+func HTMLDoc() Component {
+	return NewComponent(`<html></html>`)
 }
 
-func Div(attr string, content ...string) string {
-	return fmt.Sprintf(`<div %s>%s</div>`, attr, strings.Join(content, "\n"))
+func Div() Component {
+	return NewComponent(`<div></div>`)
 }
 
-func Header(attr string, content ...string) string {
-	return fmt.Sprintf(`<header %s>%s</header>`, attr, strings.Join(content, "\n"))
+func Header() Component {
+	return NewComponent(`<header></header>`)
 }
 
-func Li(attr string, content ...string) string {
-	return fmt.Sprintf(`<li %s>%s</li>`, attr, strings.Join(content, "\n"))
+func Li() Component {
+	return NewComponent(`<li></li>`)
 }
 
-func Main(attr string, content ...string) string {
-	return fmt.Sprintf(`<main %s>%s</main>`, attr, strings.Join(content, "\n"))
+func Main() Component {
+	return NewComponent(`<main></main>`)
 }
 
-func Nav(attr string, content ...string) string {
-	return fmt.Sprintf(`<nav %s>%s</nav>`, attr, strings.Join(content, "\n"))
+func Nav() Component {
+	return NewComponent(`<nav></nav>`)
 }
 
-func Ol(attr string, items ...string) string {
-	return fmt.Sprintf(`<ol %s>%s</ol>`, attr, strings.Join(items, "\n"))
+func Ol() Component {
+	return NewComponent(`<ol></ol>`)
 }
 
-func Script(attr string, text string) string {
-	return fmt.Sprintf(`<script %s>%s</script>`, attr, text)
+func Script() Component {
+	return NewComponent(`<script></script>`)
 }
 
-func Ul(attr string, items ...string) string {
-	return fmt.Sprintf(`<ul %s>%s</ul>`, attr, strings.Join(items, "\n"))
+func Ul() Component {
+	return NewComponent(`<ul></ul>`)
 }
 
-func Article(attr string, content ...string) string {
-	output := strings.Join(content, "\n")
-	return fmt.Sprintf(`<article %s>%s</article>`, attr, output)
+func Article() Component {
+	return NewComponent(`<article></article>`)
 }
 
-func Body(attr string, content ...string) string {
-	return fmt.Sprintf(`<body %s>%s</body>`, attr, strings.Join(content, "\n"))
+func Body() Component {
+	return NewComponent(`<body></body>`)
 }
 
 //========================================
 // ATTRIBUTES
 //========================================
 
-func Attr(funcs ...string) string {
+func Attr(attributes ...string) string {
 	output := ""
-	for _, str := range funcs {
+	for _, str := range attributes {
 		output = output + str
 	}
 	return output
--- gsc_test.go
+++ gsc_test.go
@@ -1,9 +1,16 @@
 package gsc
 
 import (
+	"fmt"
 	"testing"
 )
 
 func Test_G(t *testing.T) {
-
+	c := HTMLDoc().Content(
+		Div().Content(
+			P().Class("text-sm"),
+			H1().Class("text-xl font-bold"),
+		),
+	)
+	fmt.Println(c)
 }
